810afd87b1d9e95eabfa22736361e2281107048c
Ivan Zhao <ivanzhao8@gmail.com>
2024-08-07 23:51:17 -0700

    update text

--- src/app/page.js
+++ src/app/page.js
@@ -4,19 +4,18 @@ import styles from "./page.module.css";
 import { useEffect, useState } from "react";
 
 const text = `in the wake of waxing crescent grand // mother asks me if i have a
-        girlfriend // i respond with an assemblage of stones in my throat //
-        masquerading as language // my brother has
-        the luxury of compulsive
-        heterosexuality // i stare at the dirty ground which has never reflected
-        the moon // i peel a tomato between my thumb and fore // finger and suck
-        the juice between my tongues // seed after seed drowns in my throat as
-        we bask in fried fish and scheming alliums // the fish eye drowns me in
-        it's knowingness // my brother is having a wedding parade // he
-        feigns cultural understanding and rewarded with red envelopes // when i
-        bring my {} i am rewarded with malfeasance and gravestones // oh
-        how i would love to lick the salt off those envelopes // to taste
-        fingerdust and flaky skin // to bring him around with no questions //
-        to tell them how i've kissed the sky`;
+girlfriend // i respond with an assemblage of stones in my throat //
+masquerading as language // my brother has the luxury of compulsive
+heterosexuality // i stare at the dirty ground which has never reflected
+the moon // i peel a tomato between my thumb and fore // finger and suck
+the juice between my tongues // seed after seed stick in my lungs as
+we bask in fried fish and scheming alliums // the fish eye drowns me in
+it's knowingness // my brother is having a wedding parade // he
+feigns cultural understanding and is rewarded with red envelopes // when i
+bring {} i am rewarded with malfeasance and gravestones // oh
+how i would love to lick the salt off those envelopes // to taste
+fingerdust and flaky skin // to bring him around with no questions //
+to tell them how i've kissed the sky`;
 
 export default function Home() {
   const [opened, setOpened] = useState(false);
