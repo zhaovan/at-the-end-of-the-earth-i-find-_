b83b08260e602f30273c86a1c01ecc262d47c3db
Ivan Zhao <ivanzhao8@gmail.com>
2024-08-07 23:35:31 -0700

    adds sound effect and enter

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,7 @@
+"use client";
 import Image from "next/image";
 import styles from "./page.module.css";
+import { useEffect, useState } from "react";
 
 const text = `in the wake of waxing crescent grand // mother asks me if i have a
         girlfriend // i respond with an assemblage of stones in my throat //
@@ -17,42 +19,60 @@ const text = `in the wake of waxing crescent grand // mother asks me if i have a
         to tell them how i've kissed the sky`;
 
 export default function Home() {
+  const [opened, setOpened] = useState(false);
+  useEffect(() => {
+    const audio = new Audio("/bg.mp3");
+    audio.volume = 0.15;
+    document.addEventListener("mousemove", () => {
+      audio.play();
+    });
+  }, []);
+
   return (
     <main className={styles.main}>
-      <div className={styles.imageContainer}>
-        <Image
-          src={"/image.png"}
-          width={600}
-          height={450}
-          className={styles.image1}
-        />
-        <Image
-          src={"/image2.png"}
-          width={600}
-          height={450}
-          className={styles.image2}
-        />
-      </div>
-      <p>
-        {text.split("//").map((line, idx) => {
-          const animationDelayValue = idx * 10;
-          const top = (idx + 1) * 5;
-          const left = Math.random() * 75;
-          return (
-            <span
-              className={styles.fadingText}
-              key={idx}
-              style={{
-                animationDelay: `${animationDelayValue}s`,
-                top: `${top}vh`,
-                left: `${left}vw`,
-              }}
-            >
-              {line}
-            </span>
-          );
-        })}
-      </p>
+      {opened ? (
+        <>
+          <div className={styles.imageContainer}>
+            <Image
+              src={"/image.png"}
+              width={600}
+              height={450}
+              className={styles.image1}
+              alt="family photo"
+            />
+            <Image
+              src={"/image2.png"}
+              width={600}
+              height={450}
+              className={styles.image2}
+              priority
+              alt="family photo"
+            />
+          </div>
+          <p>
+            {text.split("//").map((line, idx) => {
+              const animationDelayValue = idx * 10;
+              const top = (idx + 1) * 5;
+              const left = Math.random() * 75;
+              return (
+                <span
+                  className={styles.fadingText}
+                  key={idx}
+                  style={{
+                    animationDelay: `${animationDelayValue}s`,
+                    top: `${top}vh`,
+                    left: `${left}vw`,
+                  }}
+                >
+                  {line}
+                </span>
+              );
+            })}
+          </p>
+        </>
+      ) : (
+        <button onClick={() => setOpened(true)}>listen...</button>
+      )}
     </main>
   );
 }
